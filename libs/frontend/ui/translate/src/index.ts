aa6f58e9a2c930ca5a7c3e0963570780223e256b
JCM <jcl.maquinay@gmail.com>
2024-10-30 20:16:35 +0100

    add translate utilities

---
+++ libs/frontend/ui/translate/src/index.ts
@@ -0,0 +1,6 @@
+
+export * from './lib/language-selector/language-selector.component';
+export * from './lib/lib.routes';
+export * from './lib/services/i18n-translate/i18n.service';
+export * from './lib/services/translations.service';
+
