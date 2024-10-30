d33cfd137079188ac0b34f84e482bed4715aa807
JCM <jcl.maquinay@gmail.com>
2024-10-30 17:23:31 +0100

    add frontend utilities

---
+++ libs/frontend/ui/i18n/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+
+export const languageSelectorRoutes: Route[] = [
+  { path: '', component: LanguageSelectorComponent },
+];
