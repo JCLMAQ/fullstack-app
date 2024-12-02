3f93a025a025d3aa6b6e34141658530eb18c3508
JCM <jcl.maquinay@gmail.com>
2024-12-02 18:06:02 +0100

    Start changes for Responsive app

--- apps/frontend/jcm-app/src/app/app.component.ts
+++ apps/frontend/jcm-app/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal, viewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { MATERIAL } from '@fe/material';
 import { CustomSidenavComponent } from './components/custom-sidenav/custom-sidenav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ResponsiveService } from './services/responsive.service';
 
 @Component({
   standalone: true,
@@ -17,7 +19,24 @@ import { HeaderComponent } from './components/header/header.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  readonly sidenav = viewChild.required(MatSidenav);
+  responsiveService = inject(ResponsiveService);
+
   title = 'jcm-app';
   collapsed = signal(false);
   sidenavWidth = computed(() => (this.collapsed() ? '65px' : '250px'));
+
+  themeSelectorMode = computed(() => {
+    if(this.responsiveService.largeWidth() ) {
+      return 'side';
+  }
+      return 'over'
+  })
+
+  componentSelectorMode = computed(() => {
+    if(this.responsiveService.smallWidth()) {
+      return 'over';
+    }
+      return 'side';
+  });
 }
