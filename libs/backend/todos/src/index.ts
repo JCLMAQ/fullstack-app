c9080ebdbeff562d5fb5fc4ac35dfcf9a3125a0a
JCM <jcl.maquinay@gmail.com>
2025-10-11 17:14:24 +0200

    Cleaning

--- libs/backend/todos/src/index.ts
+++ libs/backend/todos/src/index.ts
@@ -1,6 +1,6 @@
-export * from './lib/entities/todo.entity';
 export * from './lib/dto/create-todo.dto';
 export * from './lib/dto/update-todo.dto';
+export * from './lib/entities/todo.entity';
 export * from './lib/todos.controller';
 export * from './lib/todos.module';
 export * from './lib/todos.service';
