ff06d7f12f4ca0e0905ac2e4c700eeff840b6bf2
JCM <jcl.maquinay@gmail.com>
2025-10-11 16:42:05 +0200

    Debug

--- libs/backend/tasks/src/index.ts
+++ libs/backend/tasks/src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/entities/task.entity';
 export * from './lib/dto/create-task.dto';
 export * from './lib/dto/update-task.dto';
-export * from './lib/entities/task.entity';
 export * from './lib/tasks.controller';
 export * from './lib/tasks.module';
 export * from './lib/tasks.service';
