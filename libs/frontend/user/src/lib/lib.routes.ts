f8d447d1de1a9d8bd9d4c096060979280d5f31c9
JCM <jcl.maquinay@gmail.com>
2025-10-23 12:32:59 +0200

    Debug

--- libs/frontend/user/src/lib/lib.routes.ts
+++ libs/frontend/user/src/lib/lib.routes.ts
@@ -1,4 +1,10 @@
 import { Route } from '@angular/router';
+import { UserProfile } from './user-profile/user-profile';
 import { UserComponent } from './user/user.component';
 
-export const userRoutes: Route[] = [{ path: '', component: UserComponent }];
+// export const userRoutes: Route[] = [{ path: '', component: UserComponent }];
+
+export const userRoutes: Route[] = [
+  { path: '', component: UserComponent},
+  { path: 'userprofile', component: UserProfile },
+];
